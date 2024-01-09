bc2d5262439b802778f96e7860ab4e5a334b5027
Sakura Ryoko <sakura@ryoko.soy>
2024-01-09 17:24:06 -0500

    0.0.2 - Gson/config intro 101

--- src/main/java/io/github/sakuraryoko/corelib/config/CoreConfig.java
+++ src/main/java/io/github/sakuraryoko/corelib/config/CoreConfig.java
@@ -2,21 +2,30 @@ package io.github.sakuraryoko.corelib.config;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import io.github.sakuraryoko.corelib.config.main.MainConfig;
 import io.github.sakuraryoko.corelib.config.nodes.MoreColorConfig;
 import io.github.sakuraryoko.corelib.util.CoreLog;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CoreConfig {
-    private static final Gson CoreConfig = new GsonBuilder().create();
+    private static final List<String> CoreJson = new ArrayList<>();
     public static void init() {
+        final Gson Core = new Gson();
         // Loads default config
         MainConfig.init();
-        CoreConfig.toJson(MainConfig.modName);
+        CoreJson.add(Core.toJson(MainConfig.modName));
         MoreColorConfig.init();
-        CoreConfig.toJson(MoreColorConfig.COLORS, List.class);
-        CoreLog.info("GSON:\n"+ CoreConfig.toString());
+        CoreJson.add(String.valueOf(Core.toJsonTree(MoreColorConfig.COLORS.toArray())));
+        CoreLog.info("GSON (0):\n"+ CoreJson.get(0));
+        CoreLog.info("GSON (1):\n"+ CoreJson.get(1));
     }
     public static void load() {
 
